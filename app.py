b8cb7980002c0377660b2f40308e19d95cf62447
web <nik.ogay.03@mail.ru>
2021-10-01 18:48:46 +0300

    День 2

--- app.py
+++ app.py
@@ -1,14 +1,34 @@
-from flask import Flask, render_template, redirect, request
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config["SQLALCNEMY_DATABASE_URI"] = 'sqlite:///resulte.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///result.db'
 db = SQLAlchemy(app)
 
 
-@app.route('/news')
-def index():
-    return render_template('index.html')
+class News(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    zag = db.Column(db.String(126), nullable=False)
+    tex = db.Column(db.Text, nullable=False)
+
+
+@app.route('/write', methods=['GET', 'POST'])
+def write():
+    if request.method == 'POST':
+        a = 'Заголовок: ' + request.form.get('Tem')
+        prob = 'Текст: '
+        b = request.form.get('Mes')
+        c = News(zag=a, tex=b)
+        db.session.add(c)
+        db.session.commit()
+        return redirect('/view')
+    return render_template('write.html')
+
+
+@app.route('/view')
+def view():
+    a = News.query.order_by(News.id).all()
+    return render_template('view.html', a=a)
 
 
 app.run(host='127.0.0.1', debug=True)
